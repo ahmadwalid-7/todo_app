7c5a1e4a24f278dcc4b2c2031d174fc2ee96bc0c
AhmadWalid <ahmadcr762@gmail.com>
2026-02-17 06:28:45 +0300

    Fix gitignore and remove exposed config

--- lib/config/supa_config.dart
+++
@@ -1,4 +0,0 @@
-class SupabaseConfig {
-  static const String url = "https://irzopzqfyccpucthijgf.supabase.co";
-  static const String anonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlyem9wenFmeWNjcHVjdGhpamdmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg0MTA2NzEsImV4cCI6MjA4Mzk4NjY3MX0.RKKyjFbNS8tPsh0fUkQvm9YKD7GSGCugaDn993b_g3w";
-}
